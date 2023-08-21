06fa796322fe7a1086a7ca545e1ce88a20c9246b
haker <haker@pop-os.localdomain>
2023-08-22 00:18:58 +0400

    train, save and load model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 ## Build model
+import numpy as np
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
@@ -69,3 +70,85 @@ loss_fn = nn.L1Loss()
 
 
 #Optimizer
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.01)
+
+
+epochs = 1000
+epoch_arr = []
+loss_arr = []
+test_loss_arr = []
+
+## Training
+
+for epoch in range(epochs):
+
+  model_0.eval()
+  with torch.inference_mode():
+    test_pred = model_0.forward(x_test)
+    test_loss_arr.append(loss_fn(test_pred, y_test))
+
+  epoch_arr.append(epoch)
+  model_0.train()
+
+  y_pred = model_0(x_train)
+
+  loss = loss_fn(y_pred,y_train)
+
+  optimizer.zero_grad()
+
+  loss.backward()
+
+  optimizer.step()
+
+  model_0.eval()
+
+  print(model_0.state_dict())
+  print("loss=",loss)
+  loss_arr.append(loss)
+
+with torch.inference_mode():
+  test_pred = model_0.forward(x_test)
+  loss = loss_fn(test_pred,y_test)
+  print("test loss =", loss)
+
+
+# Plot the loss curves
+
+plt.plot(epoch_arr, np.array(torch.tensor(loss_arr).numpy()), label="Training loss")
+plt.plot(epoch_arr, np.array(torch.tensor(test_loss_arr).numpy()), label="Test loss")
+plt.title("training and test loss curves")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.legend()
+
+## SAVING model
+
+from pathlib import Path
+
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+MODEL_NAME="model_0.pth"
+MODEL_SAVE_PATH=MODEL_PATH/MODEL_NAME
+
+print(f"Saving model to {MODEL_SAVE_PATH}")
+torch.save(obj=model_0.state_dict(),
+           f=MODEL_SAVE_PATH)
+
+
+## LOADING the model
+print(model_0.state_dict())
+
+loaded_model_0 = LinearRegressionModel()
+
+loaded_model_0.load_state_dict(torch.load(f=MODEL_SAVE_PATH))
+
+print("loaded model state dict => ",loaded_model_0.state_dict())
+
+loaded_model_0.eval()
+
+with torch.inference_mode():
+  loaded_pred = loaded_model_0(x_test)
+
+print("loaded pred =>",loaded_pred)
+
+print("check if model and loaded model preds are equal =>",loaded_pred == y_preds)
